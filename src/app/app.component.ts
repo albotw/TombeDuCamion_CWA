14b734a31e89a59c3fba8172690a9fdbf7cb96f3
albotw <16798879+albotw@users.noreply.github.com>
2021-10-18 10:18:46 +0200

    changement framework
    suppression material angular
    ajout clarity design

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { MatListModule } from '@angular/material/list';
-import { MatListOption } from '@angular/material/list'
 
 @Component({
   selector: 'app-root', // ? nom de la balise lorsque l'on veut utiliser le composant
@@ -14,27 +12,20 @@ export class AppComponent implements OnInit
   text: string = "";
   title: string = "TombeDuCamion";
 
-  collaborateurs : {idx: number, nom:string, fonction :string, ambition:string}[] = [
-    {idx:0, nom: 'Yann TROU',                fonction:'Chef de Projet',              ambition: 'Staline incarné'},
-    {idx:1, nom: 'Matthieu JOULAIN',         fonction:'Codeur',                      ambition: 'AD Carry programmation'},
-    {idx:2, nom: 'Jonathan MARTIN-MAESTRE',  fonction:'Responsable Communication',   ambition: '?'},
-    {idx:3, nom: 'Justin BIZOUARD',          fonction:'Responsable Qualité',         ambition: '10E'},
-    {idx:4, nom: 'Axel TOUSSENEL',           fonction:'Codeur',                      ambition: 'le S tu connais'},
-    {idx:5, nom: 'Wassim DJELLAT',           fonction:'Codeur',                      ambition: 'futur chomeur'},
-    {idx:6, nom: 'Arthur NICOLLE',           fonction:'Stagiaire',                   ambition: 'le stagiaire de 3ème'},
+  collaborateurs: { idx: number, nom: string, fonction: string, ambition: string }[] = [
+    { idx: 0, nom: 'Yann TROU', fonction: 'Chef de Projet', ambition: 'Staline incarné' },
+    { idx: 1, nom: 'Matthieu JOULAIN', fonction: 'Codeur', ambition: 'AD Carry programmation' },
+    { idx: 2, nom: 'Jonathan MARTIN-MAESTRE', fonction: 'Responsable Communication', ambition: '?' },
+    { idx: 3, nom: 'Justin BIZOUARD', fonction: 'Responsable Qualité', ambition: '10E' },
+    { idx: 4, nom: 'Axel TOUSSENEL', fonction: 'Codeur', ambition: 'le S tu connais' },
+    { idx: 5, nom: 'Wassim DJELLAT', fonction: 'Codeur', ambition: 'futur chomeur' },
+    { idx: 6, nom: 'Arthur NICOLLE', fonction: 'Stagiaire', ambition: 'le stagiaire de 3ème' },
   ]
 
-  collab : {idx: number, nom:string, fonction :string, ambition:string} |undefined;
+  collab: { idx: number, nom: string, fonction: string, ambition: string } | undefined;
   constructor()
   {
 
-  }
-
-  onGroupsChange(options: MatListOption[]) {
-    // map these MatListOptions to their values
-    let i = options.map(o => o.value)[0];
-
-    this.collab = this.collaborateurs[i];
   }
 
   ngOnInit()
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { MatListModule } from '@angular/material/list';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ClarityModule } from '@clr/angular';
 
 @NgModule({
   declarations: [
@@ -13,8 +13,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    MatListModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    ClarityModule
   ],
   providers: [],
   bootstrap: [AppComponent]
