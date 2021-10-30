411214aebc07de1046173c5ca9cf48e0a3cf6dd4
albotw <yann.trou@gmail.com>
2021-10-30 22:16:24 +0200

    fix backend ?

--- API/src/index.ts
+++ API/src/index.ts
@@ -2,24 +2,29 @@ import Ajv from "ajv";
 import express from "express";
 import { IProduct } from "./IProduct";
 import product_schema from "./schema/product_schema.json";
-import { graphqlHTTP } from "express-graphql";
-import { buildSchema, graphql } from "graphql"
-
-import { query } from "./query";
-
-let qlschema = buildSchema(query);
 
 let app = express();
 let ajv = new Ajv();
-
-app.use("/graphql",
-    graphqlHTTP({
-        schema: gQLSchema,
-        graphiql: true
-    }));
 
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () =>
 {
     console.log("Serveur à l'écoute sur " + PORT);
 });
+
+
+
+app.use(express.json());
+//=========================================================
+// ! FONCTIONS VALIDATION
+// let validateProducts = ajv.compile<IProduct>(product_schema);
+//=========================================================
+// ! JSON LOAD
+const products = require("../JSON/products.json");
+//=========================================================
+//! FONCTIONS REST
+app.get("/products", (req, res) =>
+{
+    res.status(200);
+    res.json(products);
+})
