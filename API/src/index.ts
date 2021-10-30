f5d7a67b882ba9aa7a2be8fca0cb53c7142063f2
albotw <yann.trou@gmail.com>
2021-10-30 22:37:20 +0200

    fix api ?

--- API/src/index.ts
+++ API/src/index.ts
@@ -1,12 +1,10 @@
-import Ajv from "ajv";
 import express from "express";
-import { IProduct } from "./IProduct";
+import { IProduct } from "./interfaces/IProduct";
 import product_schema from "./schema/product_schema.json";
 
 let app = express();
-let ajv = new Ajv();
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 9090;
 app.listen(PORT, () =>
 {
     console.log("Serveur à l'écoute sur " + PORT);
@@ -15,16 +13,18 @@ app.listen(PORT, () =>
 
 
 app.use(express.json());
-//=========================================================
-// ! FONCTIONS VALIDATION
-// let validateProducts = ajv.compile<IProduct>(product_schema);
-//=========================================================
-// ! JSON LOAD
 const products = require("../JSON/products.json");
-//=========================================================
-//! FONCTIONS REST
+
+app.get("/", (req, res) =>
+{
+    res.status(200);
+    res.json({ "message": "Hello, World !" });
+})
+
 app.get("/products", (req, res) =>
 {
     res.status(200);
     res.json(products);
 })
+
+app.use
