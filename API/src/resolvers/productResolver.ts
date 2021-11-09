337df8e084b7a2e8cb16d0dc49bcd10ac1d3c9d6
albotw <16798879+albotw@users.noreply.github.com>
2021-11-09 11:53:42 +0100

    ajout notes de réunion 2

--- API/src/resolvers/productResolver.ts
+++ API/src/resolvers/productResolver.ts
@@ -5,48 +5,43 @@ let toUnicode = (toConvert: string) =>
     return toConvert.toLowerCase().normalize("NFD").replace(/[\u0300-\u036f]/g, "");
 }
 
-let productResolver = {
-    Query:
+export default {
+    products: () =>
     {
-        products: () =>
-        {
-            return products;
-        },
-        productsSearch: (param: any) =>
-        {
+        return products;
+    },
+    productsSearch: (param: any) =>
+    {
 
-            param = toUnicode(param.searchString);
-            console.log("SEARCH product with arg %o", param);
+        param = toUnicode(param.searchString);
+        console.log("SEARCH product with arg %o", param);
 
-            let output = products.filter(product =>
-            {
-                let titleAsUnicode = toUnicode(product.title);
-                let descriptionAsUnicode = toUnicode(product.description);
-                if (titleAsUnicode.includes(param) || descriptionAsUnicode.includes(param))
-                    return product;
-            }
-            );
-
-            return output;
-        },
-        product: (args: any) =>
+        let output = products.filter(product =>
         {
-            let p_uid = args.p_uid;
-            console.log("GET product with p_uid %o", p_uid);
-            return products.find(product => product.p_uid == p_uid);
-        },
-        bestSellers: () =>
-        {
-            //TODO: demander à justin une méthode efficace.
-            let bestsellers = [];
-            for (let i = 0; i < 6; i++)
-            {
-                bestsellers.push(products[i]);
-            }
-
-            return bestsellers;
+            let titleAsUnicode = toUnicode(product.title);
+            let descriptionAsUnicode = toUnicode(product.description);
+            if (titleAsUnicode.includes(param) || descriptionAsUnicode.includes(param))
+                return product;
         }
+        );
+
+        return output;
+    },
+    product: (args: any) =>
+    {
+        let p_uid = args.p_uid;
+        console.log("GET product with p_uid %o", p_uid);
+        return products.find(product => product.p_uid == p_uid);
+    },
+    bestSellers: () =>
+    {
+        //TODO: demander à justin une méthode efficace.
+        let bestsellers = [];
+        for (let i = 0; i < 6; i++)
+        {
+            bestsellers.push(products[i]);
+        }
+
+        return bestsellers;
     }
 }
-
-export default productResolver;
--- client/bin/launchapi.js
+++ client/bin/launchapi.js
@@ -1,6 +1,6 @@
 #! /usr/bin/env node
 var shell = require("shelljs")
-shell.exec("echo shell.exec works !");
 shell.cd("..");
 shell.cd("API");
+shell.echo("moved to API directory");
 shell.exec("gulp launch");
