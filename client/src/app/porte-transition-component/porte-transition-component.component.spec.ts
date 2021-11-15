6cf4fbe277fa0c4332a75c5aa12d1434de22b4a8
juju21555 <uncompte21@gmail.com>
2021-11-15 11:19:48 +0100

    correction tests

--- client/src/app/porte-transition-component/porte-transition-component.component.spec.ts
+++ client/src/app/porte-transition-component/porte-transition-component.component.spec.ts
@@ -1,25 +1,3 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { PorteTransitionComponentComponent } from './porte-transition-component.component';
-
-describe('PorteTransitionComponentComponent', () => {
-  let component: PorteTransitionComponentComponent;
-  let fixture: ComponentFixture<PorteTransitionComponentComponent>;
-
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ PorteTransitionComponentComponent ]
-    })
-    .compileComponents();
-  });
-
-  beforeEach(() => {
-    fixture = TestBed.createComponent(PorteTransitionComponentComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
-
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
-});
