c49194876c820f84e3953172bee80c851f84ed5f
albotw <16798879+albotw@users.noreply.github.com>
2021-11-16 14:02:24 +0100

    revert produits + pagination
    Ajout offset, limite dans les appels de recherche pour la pagination.
    renommage infos -> catégories
    TODO: fix frontend pour avoir les résultats

--- API/src/resolvers/productResolver.ts
+++ API/src/resolvers/productResolver.ts
@@ -12,33 +12,33 @@ let toUnicode = (toConvert: string) =>
 let saveProducts = () =>
 {
     let productString = JSON.stringify(products, null, 4);
-    fs.writeFileSync("JSON/products_v2.json", productString, { encoding: "utf-8", flag: "w" });
+    fs.writeFileSync("JSON/products.json", productString, { encoding: "utf-8", flag: "w" });
     console.log("--- Saved products ---");
 }
+
 export default {
     getAllProducts: () =>
     {
         return products;
     },
 
-    searchProduct: ({ searchString }) =>
+    searchProduct: ({ searchString, limit, offset }) =>
     {
-        console.log("searchString: " + searchString);
         searchString = toUnicode(searchString);
 
 
         let output = [];
-        
-        for (var key in products){
-            if (products.hasOwnProperty(key)){
-                
-                let product = products[key];
-    
-                let titleAsUnicode = toUnicode(product.title);
-                let descriptionAsUnicode = toUnicode(product.description);
-                if (titleAsUnicode.includes(searchString) || descriptionAsUnicode.includes(searchString)){
-                    output.push(product);
-                }
+
+        for (var i = offset; i < limit; i++)
+        {
+
+            let product = products[i];
+
+            let titleAsUnicode = toUnicode(product.title);
+            let descriptionAsUnicode = toUnicode(product.description);
+            if (titleAsUnicode.includes(searchString) || descriptionAsUnicode.includes(searchString))
+            {
+                output.push(product);
             }
         }
         return output;
@@ -46,24 +46,44 @@ export default {
 
     getProduct: ({ p_uid }) =>
     {
-        return products[p_uid];
+        return products.find(product => product.p_uid == p_uid);
     },
 
     top: ({ categorie, champ }) =>
     {
+        let topProducts = products.sort((product1, product2) =>
+        {
+            switch (champ)
+            {
+                case "sales":
+                    return product2.sells - product1.sells;
+                case "notation":
+                    return product2.notation - product1.notation;
+                case "views":
+                    return product2.views - product1.views;
+            }
+        });
 
-        let topProducts = [];
-        champ = 'best_'+champ;
-        
-        for (var p_uid in infos[champ]){
-            topProducts.push(products[infos[champ][p_uid]]);
-        }
-        return topProducts;
+        let filteredTopProducts = topProducts.reduce((output, product) =>
+        {
+            if (output.length < 6)
+            {
+                if (categorie != "Global" && product.category == categorie)
+                {
+                    output.push(product);
+                }
+                else if (categorie == "Global")
+                {
+                    output.push(product);
+                }
+            }
+            return output;
+        }, []);
+        return filteredTopProducts;
     },
 
     createProduct: ({ seller, title, stock, description, category, price }) =>
     {
-        console.log("create");
         //TODO: ajouter import images: placement dans IMG/, hasher nom fichier, ajouter hash dans [images]
         //TODO: vérifier catégorie
         title = title.normalize("NFD");
@@ -85,7 +105,7 @@ export default {
             views: 0
         };
 
-        console.log(product);
+        // ? placé au début des produits pour débug facile.
         products.unshift(product);
 
         saveProducts();
