d3ea16f5eab581ee404c38b61498228493caba7c
Hahiro <34438643+PyHahiro@users.noreply.github.com>
2022-01-07 00:03:23 +0100

    loading items, merging new changes fixed

--- client/src/app/components/pages/compte/compte-achat/compte-achat.component.ts
+++ client/src/app/components/pages/compte/compte-achat/compte-achat.component.ts
@@ -2,7 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import {MatPaginator} from '@angular/material/paginator';
 import {MatTableDataSource} from '@angular/material/table';
 import { ActivatedRoute, Router } from '@angular/router';
+import { PageEvent } from '@angular/material/paginator';
 import DataController from '../../../../shared/DataController';
+import { data } from '../../../../shared/global'
 
 @Component({
   selector: 'app-compte-achat',
@@ -10,35 +12,45 @@ import DataController from '../../../../shared/DataController';
   styleUrls: ['./compte-achat.component.css']
 })
 export class CompteAchatComponent implements OnInit {
-  product_bought = [];
-  totalCount = 0;
-  constructor(private route: ActivatedRoute, private router: Router)
+	products = [];
+	totalCount = 0;
+	actualSort = "NO_SORT";
+	DATA = data;
+	pageIndex = 0;
+	pageSize = 16;	
+	constructor(private route: ActivatedRoute, private router: Router)
 	{
 		this.router.routeReuseStrategy.shouldReuseRoute = () => false;
 	}
 
-  ngOnInit(): void {
-    this.refreshProducts(0, 20);
-  }
+	ngOnInit(): void {
+	this.refreshProducts(this.pageIndex, this.pageSize);
+	}
 
-  public refreshProducts(pageIndex: number, pageSize: number)
+	public refreshProducts(pageIndex: number, pageSize: number)
 	{
-
 		let offset = pageIndex * pageSize;
 
-		DataController.searchProduct("", pageSize, offset, (data) =>
+		DataController.searchProduct("","", pageSize, offset, this.DATA.actualSort, this.DATA.filter, (data) =>
 		{
 			this.totalCount = data.meta.totalCount;
-			this.product_bought = data.results.map(product =>
+			this.products = data.results.map(product =>
 			{
 				if (product['description'].length > 40)
 				{
 					product['description'] = product['description'].substring(0, 37) + "...";
 				}
-
 				return product;
 			})
+			print()
 		});
+	}
+
+	public getServerData(event?: PageEvent)
+	{
+		this.pageIndex = event.pageIndex;
+		this.pageSize = event.pageSize;
+		this.refreshProducts(event.pageIndex, event.pageSize);
 	}
 
 }
