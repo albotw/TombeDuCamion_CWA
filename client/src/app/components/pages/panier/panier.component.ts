104ba7c26c2e355d272e2335cee9177933646749
djellatine <wassim52@hotmail.fr>
2022-01-07 14:40:25 +0100

    Panier panier

--- client/src/app/components/pages/panier/panier.component.ts
+++ client/src/app/components/pages/panier/panier.component.ts
@@ -25,22 +25,24 @@ export class PanierComponent implements OnInit
 
 	}
 
-	ControlRemoveProduct = (product) => {
+	ControlRemoveProduct (product) : void {
 		this.panier.RemoveFromTab(product);
 		this.TabProducts = this.panier.getProductFromTab();
 		this.ProductTotalValeur = this.panier.getTotalPanier();
 	}
 
-	ControlIncrement = (product) => {
+	ControlIncrement (product) : void  {
 		this.panier.addProductsToTab(product);
 		this.TabProducts = this.panier.getProductFromTab();
 		this.ProductTotalValeur = this.panier.getTotalPanier();
 	}
 
-	ControlDecrement = (product) => {
+	ControlDecrement (product) : void {
+		console.log("jfgerf");
 		this.panier.MoinsFromTab(product);
 		this.TabProducts = this.panier.getProductFromTab();
 		this.ProductTotalValeur = this.panier.getTotalPanier();
+		
 	}
 
 
