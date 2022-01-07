a04b7fae11101b41e8c864a9be549af920aefa03
juju21555 <uncompte21@gmail.com>
2022-01-07 19:36:40 +0100

    omg le push de jojo fonctionne ?

--- client/src/app/components/pages/compte/compte-achat/compte-achat.component.spec.ts
+++ client/src/app/components/pages/compte/compte-achat/compte-achat.component.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 
 import { CompteAchatComponent } from './compte-achat.component';
 
@@ -8,6 +9,7 @@ describe('CompteAchatComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [RouterTestingModule],
       declarations: [ CompteAchatComponent ]
     })
     .compileComponents();
--- client/src/app/components/pages/compte/compte-vente/compte-vente.component.spec.ts
+++ client/src/app/components/pages/compte/compte-vente/compte-vente.component.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 
 import { CompteVenteComponent } from './compte-vente.component';
 
@@ -8,6 +9,7 @@ describe('CompteVenteComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [RouterTestingModule],
       declarations: [ CompteVenteComponent ]
     })
     .compileComponents();
